651c38e3cacf7d21ddbde2449df791422e0eba51
Kelley Shann <kelleyshann@gmail.com>
2019-10-14 18:57:21 -0400

    Feature: Added age logic code

--- src/name_script.py
+++ src/name_script.py
@@ -1,3 +1,16 @@
 for letter in 'KelleyShann':
     print('The letter: ', letter)
 print('That is my name!')
+
+names = ['kelley','sarah','john','james']
+ages = [28,28,34,21]
+
+for n, a in zip(names, ages):
+        print(n, "is", a)
+
+        oldest = max(ages)
+
+        for n, a in zip(names, ages):
+                if a == oldest:
+                            print(n, "is the oldest")
+
