c883c8c631b8dbb2c4e6392d36cfebea98091860
jeanshanchik <kelleyshann@gmail.com>
2019-10-14 18:59:33 -0400

    Fix: edited indentations

--- src/name_script.py
+++ src/name_script.py
@@ -1,16 +1,19 @@
 for letter in 'KelleyShann':
     print('The letter: ', letter)
+    
 print('That is my name!')
 
+#defining name and age lists
 names = ['kelley','sarah','john','james']
 ages = [28,28,34,21]
 
 for n, a in zip(names, ages):
-        print(n, "is", a)
+    print(n, "is", a)
 
-        oldest = max(ages)
+#determining who the oldest is
+oldest = max(ages)
 
-        for n, a in zip(names, ages):
-                if a == oldest:
-                            print(n, "is the oldest")
+for n, a in zip(names, ages):
+    if a == oldest:
+    print(n, "is the oldest")
 
